3068fbec155578db950673ef80b8a7f157ca2f2c
zarg336 <70839905+zarg336@users.noreply.github.com>
2020-10-25 13:44:03 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let kus = str.length / 2
+    do {
+        for (let x of bracketsConfig) {
+            str = str.replace(x[0] + x[1], '');
+        }
+        kus--
+    } while (kus > 0)
+    if (str.length === 0) { return true }
+    return false
 }
